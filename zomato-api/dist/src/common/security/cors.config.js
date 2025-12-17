0c9ec9949d776e254e01ff9a9eb569a7b718f04e
Arsh Verma <arshverma.dev@gmail.com>
2025-12-17 15:00:08 +0530

    Create cors.config.js

---
+++ zomato-api/dist/src/common/security/cors.config.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.corsMiddleware = exports.corsOptions = void 0;
+const cors_1 = __importDefault(require("cors"));
+const allowedOrigins = [
+    process.env.CUSTOMER_APP_URL || 'http://localhost:8081',
+    process.env.ADMIN_URL || 'http://localhost:3001',
+    process.env.RESTAURANT_APP_URL || 'http://localhost:8082',
+    process.env.DELIVERY_APP_URL || 'http://localhost:8083',
+    'http://localhost:3000',
+    'http://localhost:3001',
+];
+exports.corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) {
+            return callback(null, true);
+        }
+        if (allowedOrigins.includes(origin) || process.env.NODE_ENV === 'development') {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-CSRF-Token'],
+    exposedHeaders: ['X-Total-Count', 'X-Page-Count'],
+    maxAge: 86400,
+};
+exports.corsMiddleware = (0, cors_1.default)(exports.corsOptions);
+//# sourceMappingURL=cors.config.js.map
