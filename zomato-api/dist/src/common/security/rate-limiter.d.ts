d94e3dfed96f865899d56d5f929b1223d17cc4bc
Arsh Verma <arshverma.dev@gmail.com>
2025-12-17 15:01:35 +0530

    Create rate-limiter.d.ts

---
+++ zomato-api/dist/src/common/security/rate-limiter.d.ts
@@ -0,0 +1,5 @@
+export declare const apiRateLimiter: any;
+export declare const authRateLimiter: any;
+export declare const otpRateLimiter: any;
+export declare const paymentRateLimiter: any;
+export declare function createRedisRateLimiter(redisClient: any, options?: any): any;
