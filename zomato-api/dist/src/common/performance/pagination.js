ea662efeaa15f6bb75f58af6d06feff8e2f7db78
Arsh Verma <arshverma.dev@gmail.com>
2025-12-17 14:59:28 +0530

    Create pagination.js

---
+++ zomato-api/dist/src/common/performance/pagination.js
@@ -0,0 +1,95 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PaginationDto = void 0;
+exports.paginate = paginate;
+exports.getPrismaSkipTake = getPrismaSkipTake;
+exports.getCursorPagination = getCursorPagination;
+const class_validator_1 = require("class-validator");
+const class_transformer_1 = require("class-transformer");
+const swagger_1 = require("@nestjs/swagger");
+class PaginationDto {
+    page = 1;
+    limit = 20;
+    cursor;
+    sortBy = 'createdAt';
+    sortOrder = 'desc';
+}
+exports.PaginationDto = PaginationDto;
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ default: 1 }),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_transformer_1.Type)(() => Number),
+    (0, class_validator_1.IsInt)(),
+    (0, class_validator_1.Min)(1),
+    __metadata("design:type", Number)
+], PaginationDto.prototype, "page", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ default: 20 }),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_transformer_1.Type)(() => Number),
+    (0, class_validator_1.IsInt)(),
+    (0, class_validator_1.Min)(1),
+    (0, class_validator_1.Max)(100),
+    __metadata("design:type", Number)
+], PaginationDto.prototype, "limit", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)(),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)(),
+    __metadata("design:type", String)
+], PaginationDto.prototype, "cursor", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ default: 'createdAt' }),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)(),
+    __metadata("design:type", String)
+], PaginationDto.prototype, "sortBy", void 0);
+__decorate([
+    (0, swagger_1.ApiPropertyOptional)({ enum: ['asc', 'desc'], default: 'desc' }),
+    (0, class_validator_1.IsOptional)(),
+    (0, class_validator_1.IsString)(),
+    (0, class_transformer_1.Transform)(({ value }) => value?.toLowerCase()),
+    __metadata("design:type", String)
+], PaginationDto.prototype, "sortOrder", void 0);
+function paginate(data, total, pagination) {
+    const { page, limit } = pagination;
+    const totalPages = Math.ceil(total / limit);
+    return {
+        data,
+        meta: {
+            total,
+            page,
+            limit,
+            totalPages,
+            hasNext: page < totalPages,
+            hasPrev: page > 1,
+        },
+    };
+}
+function getPrismaSkipTake(pagination) {
+    const { page, limit } = pagination;
+    return {
+        skip: (page - 1) * limit,
+        take: limit,
+    };
+}
+function getCursorPagination(pagination) {
+    const { cursor, limit } = pagination;
+    return {
+        take: limit,
+        ...(cursor && {
+            skip: 1,
+            cursor: { id: cursor },
+        }),
+    };
+}
+//# sourceMappingURL=pagination.js.map
