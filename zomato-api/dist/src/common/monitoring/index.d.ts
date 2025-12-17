db6ae783cc2ff9eb9d27bd8e1d788701cd5749c6
Arsh Verma <arshverma.dev@gmail.com>
2025-12-17 13:52:53 +0530

    Create index.d.ts

---
+++ zomato-api/dist/src/common/monitoring/index.d.ts
@@ -0,0 +1,3 @@
+export { initSentry, initSentryErrorHandler, captureException, captureMessage, setUser, Sentry } from './sentry';
+export { metricsMiddleware, metricsHandler, ordersCreated, ordersCompleted, activeDeliveryPartners, activeOrders } from './metrics';
+export { HealthController } from './health.controller';
